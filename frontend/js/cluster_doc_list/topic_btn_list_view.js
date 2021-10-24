ad882e12b274fa2775644b74acb790d7c681aa10
sam <samminweng@gmail.com>
2021-10-25 09:36:27 +1300

    Added N-gram to display the topic of 1, 2, 3 gram sorted by score or count

--- frontend/js/cluster_doc_list/topic_btn_list_view.js
+++ frontend/js/cluster_doc_list/topic_btn_list_view.js
@@ -3,6 +3,8 @@ function TopicBtnListView(cluster_no, cluster_topic_words, doc_key_terms){
     // Fill out the topic using TF-IDF
     const cluster_topics = cluster_topic_words.find(c => c['Cluster'] === cluster_no);
     const n_gram_topics = cluster_topics['Topic1Grams'].concat(cluster_topics['Topic2Grams'], cluster_topics['Topic2Grams']);
+    // Sort the topics by scores
+    n_gram_topics.sort((a, b) => b['score'] - a['score']);
     const available_topics = n_gram_topics.map(t => t['topic']);
     const cluster_doc_ids = cluster_topics['DocIds'];
     const cluster_docs = doc_key_terms.filter(d => cluster_doc_ids.includes(parseInt(d['DocId'])));
@@ -30,10 +32,10 @@ function TopicBtnListView(cluster_no, cluster_topic_words, doc_key_terms){
     function createTopicListView() {
         $('#topic_list_view').empty();
         const container = $('<div class="container"></div>');
-        const display_names = {'1Grams': '1-gram', '2Grams': '2-gram', '3Grams': '3-gram'}
+        const display_names = {'1Grams': '1-gram', '2Grams': '2-gram', '3Grams': '3-gram', 'N-gram': 'N-gram'};
         // Display the topics of 1 gram, 2 grams and 3 grams
-        for(const n_gram of ['1Grams', '2Grams', '3Grams']){
-            const topics = cluster_topics['Topic' + n_gram];
+        for(const n_gram of ['1Grams', '2Grams', '3Grams', 'N-gram']){
+            const topics = n_gram !== 'N-gram' ? cluster_topics['Topic' + n_gram] : n_gram_topics;
             const key_term_div = $('<div><h3><span class="fw-bold">'+ display_names[n_gram] + ' Topics </span> ' +
                 '</h3>' +
                 '<div class="topics">' +
@@ -53,36 +55,36 @@ function TopicBtnListView(cluster_no, cluster_topic_words, doc_key_terms){
                 '</div>');
             // Add a radio button to sort topics by scores or range
             key_term_div.find(".topics").append(sort_widget);
-            key_term_div.find(".topics").append(createTopicParagraphs(topics));
+            key_term_div.find(".topics").append(createTopicParagraphs(topics.slice(0, 50))); // Show top 50 topics
             // Add the on click event to radio button
             sort_widget.find('input[name='+sort_btn_name+']').change(function(){
                 const sorted_index = sort_widget.find('input[name='+sort_btn_name+']:checked').val();
+                const sorted_topic = n_gram !== 'N-gram' ? cluster_topics['Topic' + n_gram] : n_gram_topics;
                 if(sorted_index === 'count'){
                     // Sort the topic by count
-                    topics.sort((a, b) => b['doc_ids'].length - a['doc_ids'].length );
+                    sorted_topic.sort((a, b) => b['doc_ids'].length - a['doc_ids'].length );
                 }else{
-                    topics.sort((a, b) => b['score'] - a['score'] );// Sort by score
+                    sorted_topic.sort((a, b) => b['score'] - a['score'] );// Sort by score
                 }
                 // console.log(topics);
                 key_term_div.find(".topic_list").remove();
-                key_term_div.find(".topics").append(createTopicParagraphs(topics));
-
+                key_term_div.find(".topics").append(createTopicParagraphs(sorted_topic.slice(0, 50)));
             });
 
 
-            // if(n_gram === 'Bag_of_Words'){
-            //     // Show the topics for TF-IDF
-            //     key_term_div.accordion({
-            //         collapsible: true,
-            //         heightStyle: "fill"
-            //     });
-            // }else{
+            if(n_gram === 'N-gram'){
+                // Show the topics for TF-IDF
+                key_term_div.accordion({
+                    collapsible: true,
+                    heightStyle: "fill"
+                });
+            }else{
                 key_term_div.accordion({
                     collapsible: true,
                     heightStyle: "fill",
                     active: 2
                 });
-            // }
+            }
             container.append($("<div class='row'><div class='col'></div></div>").find(".col").append(key_term_div));
         }
         $('#topic_list_view').append(container);
