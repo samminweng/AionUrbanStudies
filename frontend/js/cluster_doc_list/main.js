dc4d258c1e4d477a81ba5bc5d9ef2f4821e6f434
sam <samminweng@gmail.com>
2021-10-25 10:43:41 +1300

    Added the progress bar to indicate the loading progress

--- frontend/js/cluster_doc_list/main.js
+++ frontend/js/cluster_doc_list/main.js
@@ -6,7 +6,7 @@ const params = new URLSearchParams(window.location.search);
 function load_cluster_data_display_results(cluster_approach, cluster_topic_words, doc_key_terms) {
     // Select cluster number
     let selected_cluster_no = 2;
-    if(params.has('cluster')){
+    if (params.has('cluster')) {
         selected_cluster_no = parseInt(params.get('cluster'));
     }
 
@@ -28,15 +28,28 @@ function load_cluster_data_display_results(cluster_approach, cluster_topic_words
         const cluster_number = parseInt(this.value);
         const topic_btn = new TopicBtnListView(cluster_number, cluster_topic_words[cluster_approach], doc_key_terms);
     });
-
 }
-
 
 // Document ready event
 $(function () {
+    // Update the progress bar asynchronously 
+    $('#progressbar').progressbar({
+        value: 0,
+        complete: function() {
+            $( ".progress-label" ).text( "Complete!" );
+        }
+    });
+    let counter = 0;
+    (function asyncLoop() {
+        $('#progressbar').progressbar("value", counter++);
+        if (counter <= 100) {
+            setTimeout(asyncLoop, 100);
+        }
+    })();
+
+
     // Document (article abstract and title) and key terms data
     const doc_key_terms_file_path = 'data/doc_cluster/' + corpus + '_doc_terms.json';
-
     // HDBSCAN cluster and topic words data
     const hdbscan_cluster_topic_words_file_path = 'data/doc_cluster/' + corpus + '_HDBSCAN_Cluster_topic_words.json';
     // KMeans cluster and topic words data
@@ -45,6 +58,7 @@ $(function () {
         $.getJSON(doc_key_terms_file_path), $.getJSON(hdbscan_cluster_topic_words_file_path),
         $.getJSON(kmeans_cluster_topic_words_file_path),
     ).done(function (result1, result2, result3) {
+
         const doc_key_terms = result1[0];
         const cluster_topic_words = {"HDBSCAN": result2[0], "KMeans": result3[0]};
         // Change the cluster approach
@@ -54,12 +68,15 @@ $(function () {
                 load_cluster_data_display_results(cluster_approach, cluster_topic_words, doc_key_terms);
             }
         });
+        // Load the cluster and display the results
         load_cluster_data_display_results('HDBSCAN', cluster_topic_words, doc_key_terms);
         // Set up print /download as a pdf
         $('#download_as_pdf').button();
         $('#download_as_pdf').click(function (event) {
             window.print();
         });
+        // Remove the progress bar
+        $('#progressbar').remove();
 
     })
 });
