f015dae4d730797c36b643417b10468ebeed8db2
samminweng <samminweng@gmail.com>
2022-01-31 12:56:51 +1300

    Display term occurrence chart when clicking on a topic or phrase group

---
+++ frontend/js/term_sunburst/d3_network_graph.js
@@ -0,0 +1,190 @@
+// Create D3 network graph using collocation and
+function D3NetworkGraph(word_docs, is_key_phrase, chart_div, color) {
+    const chart_id = chart_div.attr('id');
+    // Convert the word_docs map to nodes/links
+    const {nodes, links} = create_node_link_data(word_docs);
+    // console.log(nodes);
+    // console.log(links);
+    const width = 600;
+    const height = 600;
+    const max_radius = 30;
+    let node_radius = 2;
+    let link_width = 1;
+    let forceNode = d3.forceManyBody().strength(-2000);
+    let forceLink = d3.forceLink(links).id(d => d.id);
+    if(is_key_phrase){
+        forceNode = d3.forceManyBody();
+        forceLink = d3.forceLink(links).id(d => d.id).distance(100);
+        node_radius = 5;
+        link_width = 5;
+    }
+
+    // Convert the json to the format of D3 network graph
+    function create_node_link_data(word_docs) {
+        // Convert the word_docs to a list
+        // Populate the nodes with collocation data
+        let nodes = [];
+        let id = 0;
+        // Add other nodes from term map
+        for (const [word, doc_ids] of Object.entries(word_docs) ) {
+            const node = {'id': id, 'name': word, 'doc_ids': doc_ids};
+            nodes.push(node);
+            id += 1;
+        }
+        // Sort the nodes by the doc_ids
+        nodes.sort((a, b) => a['doc_ids'].length - b['doc_ids'].length);
+        // console.log(nodes);
+        // Populate the links with occurrences
+        let links = [];
+        for (let source = 0; source < nodes.length; source++) {
+            for (let target = source + 1; target < nodes.length; target++) {
+                const source_doc_ids = nodes[source]['doc_ids'];
+                const target_doc_ids = nodes[target]['doc_ids'];
+                const occ_doc_ids = source_doc_ids.filter(doc_id => target_doc_ids.includes(doc_id));
+                // console.log(occ_doc_ids);
+                if(occ_doc_ids.length > 0){
+                    // Add the link
+                    links.push({source: nodes[source], target: nodes[target], doc_ids: occ_doc_ids,
+                                weight: occ_doc_ids.length});
+                }
+            }
+        }
+        return {nodes: nodes, links: links};
+    }
+
+    // Get the number of documents for a collocation node
+    function get_node_size(node) {
+        let num_doc = node['doc_ids'].length;
+        // return Math.round(num_doc + max_radius;
+        return Math.min(num_doc*node_radius, max_radius);
+    }
+
+    // Get the link weight
+    function get_link_width(link){
+        return link.weight * link_width;
+    }
+
+    // Drag event function
+    const drag = function (simulation) {
+
+        function dragstarted(event) {
+            if (!event.active) simulation.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+        }
+
+        function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+        }
+
+        function dragended(event) {
+            if (!event.active) simulation.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+        }
+
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+    }
+
+    // Create a network graph using D3
+    function create_d3_network_graph() {
+        // Add the svg node to 'term_map' div
+        let svg = d3.select('#'+chart_id).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("viewBox", [-width / 2, -height / 2, width, height])
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+    //     // Simulation
+        const simulation = d3.forceSimulation(nodes)
+            .force('link', forceLink)
+            .force("charge", forceNode)
+            .force('center', d3.forceCenter())
+            .on("tick", ticked);
+
+        // Initialise the links
+        let link = svg.append("g")
+                .attr("class", "link")
+                .attr("stroke", "black")
+                .attr("stroke-opacity", 0.1)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", d => get_link_width(d));
+
+        // Initialise the nodes
+        let node = svg.append('g')
+            .selectAll("g")
+            .data(nodes)
+            .join("g")
+            .call(drag(simulation));
+
+        // Add the circles
+        node.append("circle")
+            .attr("stroke", "white")
+            .attr("stroke-width", 1.5)
+            .attr("r", d => get_node_size(d))
+            .attr("fill", d => color);
+        // node.append("image")
+        //     .attr('href', d => get_image_path(d))
+        //     .attr('x', d => -1 * get_node_size(d)/2 )
+        //     .attr('y', d => -1 * get_node_size(d)/2)
+        //     .attr("stroke", "white")
+        //     .attr("stroke-width", 1.5)
+        //     .attr("width", d => get_node_size(d))
+        //     .attr("height", d => get_node_size(d))
+        //     .attr('rx', "3")
+        //     .attr("color", d => colors(d));
+
+        // Add node label
+        node.append("text")
+            .attr("class", "lead")
+            .attr('x', "1em")
+            .attr('y', "0.5em")
+            .style("font", d => "14px sans-serif")
+            .text(d => {
+                return d.name;
+            });
+        // // Tooltip
+        node.append("title").text(d => {
+            // Find the source node of d
+            const d_links = links.filter(link => link['source'].name === d.name || link['target'].name === d.name);
+            // Aggregate all the doc ids to a set to avoid duplicate doc ids
+            const occ_doc_ids = new Set();
+            for(const d_link of d_links){
+                for (const doc_id of d_link['doc_ids']){
+                    occ_doc_ids.add(doc_id);
+                }
+            }
+            return "'" + d.name + "' alone appears in " + d['doc_ids'].length + " papers and\n" +
+                        "co-occurs with " + d_links.length + " words in " + occ_doc_ids.size + " papers\n";
+        });
+
+        // Simulate the tick event
+        function ticked() {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node.attr("transform", d => `translate(${d.x},${d.y})`);
+        }
+    }
+    //
+    //
+    // Create the network graph using D3 library
+    function _createUI() {
+        try {
+            chart_div.empty(); // Clear the SVG graph
+            create_d3_network_graph();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    _createUI();
+}
--- frontend/js/term_sunburst/sunburst_graph.js
+++ frontend/js/term_sunburst/sunburst_graph.js
@@ -1,85 +1,149 @@
 // Create Plotly sunburst graph to display Grouped key phrases and LDA Topics
 // Ref: https://plotly.com/javascript/sunburst-charts/
-function SunburstGraph(key_phrase_groups, cluster_no, is_key_phrase) {
-    const chart_div = (is_key_phrase?  $('#key_phrase_chart'): $('#lda_topic_chart'));
+// Ref: https://plotly.com/javascript/reference/sunburst/
+function SunburstGraph(group_data, cluster_no, is_key_phrase) {
+    const chart_div = (is_key_phrase? $('#key_phrase_chart'): $('#lda_topic_chart'));
     // Convert the word_docs map to nodes/links
-    const {total, group_total_list} = compute_total(key_phrase_groups);
-    // console.log(total);
-    // console.log(group_total_list);
-    const {labels, values, parents} = create_graph_data();
-    console.log(labels);
-    console.log(values);
-    console.log(parents);
+    const {total, group_list} = compute_total(group_data);
+    const {labels, values, parents, ids} = create_graph_data();
+    console.log(ids);
     const width = 600;
     const height = 600;
+    const D3Colors = d3.schemeCategory10;
 
     // Count the count of a groups
     function compute_total(key_phrase_groups){
         let total = 0;
-        let group_total_list = [];
+        let group_list = [];
         for(const group of key_phrase_groups){
             let group_total = 0;
             for(const [top_word, doc_ids] of Object.entries(group['word_docIds'])){
                 group_total += doc_ids.length;
                 total += doc_ids.length;
             }
-            group_total_list.push(group_total);
+            group_list.push({group: group, total: group_total});
         }
-        return {total: total, group_total_list: group_total_list};
+        // Sort by the total
+        group_list.sort((a, b) => b['total'] - a['total'])
+        return {total: total, group_list: group_list};
     }
 
     // Convert the json to the format of plotly graph
     function create_graph_data() {
         // Populate the groups of key phrases
         const root = "Cluster#"+cluster_no;
+        let ids = [root];
         let labels = [root];
         let values = [total];
         let parents = [""];
-        for(let i=0; i < key_phrase_groups.length; i++){
+        for(let i=0; i < group_list.length; i++){
+            const group = group_list[i]['group'];
+            const group_total = group_list[i]['total'];
             const prefix = (is_key_phrase ? "Group": "Topic");
             const group_name = prefix + " #" + (i + 1);
-            labels.push(group_name);
-            values.push(group_total_list[i]);
+            const group_score = group['score'].toFixed(2);
+            ids.push(group_name);
+            labels.push(group_name + " <br>score:" + group_score);
+            values.push(group_total);
             parents.push(root);
-            // Added the top 10 word
-            const group = key_phrase_groups[i];
             const top_words = group['top_words'];
             for(const top_word of top_words){
                 const num_docs = group['word_docIds'][top_word].length;
-                labels.push(top_word);
+                const w_id =  group_name + " - " + top_word;
+                let w_label = top_word;
+                const w_array = top_word.split(" ");
+                if(w_array.length > 2){
+                    w_label = w_array[0] + " " + w_array[1] + "<br>" + w_array[2];
+                }
+                ids.push(w_id);
+                labels.push(w_label);
                 values.push(num_docs);
                 parents.push(group_name);
             }
-
-            // console.log(group);
         }
-        return {labels: labels, values: values, parents: parents};
+        return {labels: labels, values: values, parents: parents, ids:ids};
     }
-
+    // Create the sunburst
     function create_sunburst_graph() {
         chart_div.empty();
         const data = [{
             "type": "sunburst",
+            "maxdepth": 3,
+            "ids": ids,
             "labels": labels,
             "parents": parents,
             "values": values,
             "leaf": {"opacity": 0.7},
             "marker": {"line": {"width": 2}},
-            "branchvalues": 'total'
+            "branchvalues": 'total',
+            "outsidetextfont": {size: 24, color: "#377eb8"},
+            "insidetextfont": {size: 16},
+            'insidetextorientation': "horizontal",
+            // Label text orientation
+            "textposition": 'inside',
+            'hoverinfo': 'label',
         }];
+
         const layout = {
             "margin": {"l": 10, "r": 10, "b": 10, "t": 10},
+            // "sunburstcolorway": D3Colors
         };
+        const chart_id = chart_div.attr('id');
+        Plotly.newPlot(chart_id, data, layout, {showSendToCloud: true})
+        const chart_element = document.getElementById(chart_id);
 
-        Plotly.newPlot(chart_div.attr('id'), data, layout, {showSendToCloud: true})
+        // Define the hover event
+        chart_element.on('plotly_click', function(data){
+            const id = data.points[0].id;
+            if(id.startsWith('Topic')){
+                const index = parseInt(id.split("#")[1]) - 1;
+                const group = group_data[index];
+                console.log(group);
+                const word_docs = group['word_docIds'];
+                const chart_div = $('#lda_topic_network_graph');
+                const network_graph = new D3NetworkGraph(word_docs, false, chart_div, D3Colors[index]);
+            }else if(id.startsWith('Group')){
+                const index = parseInt(id.split("#")[1]) - 1;
+                const group = group_data[index];
+                console.log(group);
+                const word_docs = group['word_docIds'];
+                const chart_div = $('#phrase_network_graph');
+                const network_graph = new D3NetworkGraph(word_docs, true, chart_div, D3Colors[index]);
+            }
+
+        })
+
 
     }
-
+    // Create the header
+    function create_header(){
+        const total_score = group_data.reduce((pre, cur) => cur['score'] + pre, 0);
+        const avg_score = total_score / group_data.length;
+        const group_count = group_data.length;
+        if(is_key_phrase){
+            $('#phrase_count').text(group_count);
+            $('#phrase_score').text(avg_score.toFixed(2));
+            if(avg_score < 0){
+                $('#phrase_score').attr('class', 'text-danger');
+            }else{
+                $('#phrase_score').removeAttr("class");
+            }
+        }else{
+            $('#topic_count').text(group_count);
+            $('#topic_score').text(avg_score.toFixed(2));
+            if(avg_score < 0){
+                $('#topic_score').attr('class', 'text-danger');
+            }else{
+                $('#topic_score').removeAttr("class");
+            }
+        }
+    }
 
     // Create the sunburst graph using D3 library
     function createUI() {
         try {
             create_sunburst_graph();
+            create_header();
         } catch (error) {
             console.error(error);
         }
