6402647c4ed8f40c1d0117c10eca006eaca01678
samminweng <samminweng@gmail.com>
2022-02-10 19:21:06 +1300

    Added a bar chart to display the number of papers for a group of key phrases.

---
+++ frontend/js/term_sunburst/bar_chart.js
@@ -0,0 +1,124 @@
+function BarChart(group_data, sub_group_data, cluster, cluster_docs){
+    const d3colors = d3.schemeCategory10;
+    console.log(d3colors);
+    const min_group_id = group_data.reduce((pre, cur) => pre['Group'] < cur['Group']? pre: cur)['Group'];
+    let thread = 2;
+    if(min_group_id === 0){
+        thread = 1;
+    }
+    const top_terms = cluster['TopTerms'];
+    console.log(group_data);
+    const data = create_graph_data();
+    console.log(data);
+
+
+    // Graph data
+    function create_graph_data(){
+        let data = [];
+        // Re-order the groups to match with the order of the chart.
+        group_data.sort((a, b) => a['Group'] - b['Group']);
+        for(const group of group_data){
+            const group_id = group['Group'];
+            const group_name = "Topic#" + (group_id + thread);
+            // Get the sub-group
+            const sub_groups = sub_group_data.filter(g => g['Group'] === group_id);
+            // create a trace
+            let trace = {x: [], y:[], text: [], orientation: 'h', type: 'bar',
+                        name: group_name,
+                        textposition: 'auto',
+                        hovertemplate: '%{x} papers',
+                        marker: {
+                            color: d3colors[group_id+1]
+                        }
+                        };
+            if(sub_groups.length > 0){
+                for(const sub_group of sub_groups){
+                    console.log(sub_group);
+                    const sub_group_id = sub_group['SubGroup'];
+                    const title_words = sub_group['TitleWords'];
+                    const num_docs = sub_group['NumDocs'];
+                    trace['y'].push(group_name + "-" + sub_group_id);
+                    trace['x'].push(num_docs);
+                    trace['text'].push(title_words.join(", "))
+                }
+            }else{
+                // Add the group
+                const title_words = group['TitleWords'];
+                const num_docs = group['NumDocs'];
+                trace['y'].push(group_name + "#" + group_id);
+                trace['x'].push(num_docs);
+                trace['text'].push(title_words.join(", "))
+            }
+            data.push(trace);
+        }
+
+        return data;
+    }
+
+
+    function create_UI(){
+        const layout = {
+            title: top_terms.join(", ") + " (" + cluster_docs.length + " papers)",
+            xaxis: {
+                title: 'Number of Papers',
+            },
+            yaxis:{
+                tickfont: {
+                    size: 1,
+                }
+            },
+            width: 600,
+            height: 700,
+            showlegend: true,
+            barmode: 'group',
+            bargap : 0.4,
+            "margin": {"l": 10, "r": 10},
+            insidetextfont: {
+                size: 16
+            },
+            legend :{
+                traceorder: 'reversed',
+            }
+            // hovermode: 'closest',
+            // config: { responsive: true }
+        };
+
+        // Plot bar chart
+        Plotly.newPlot('key_phrase_chart', data, layout);
+
+        const chart_element = document.getElementById('key_phrase_chart');
+
+        // // Define the hover event
+        chart_element.on('plotly_click', function(data){
+            $('#sub_group').empty();
+            $('#doc_list').empty();
+            const id = data.points[0].y;
+            console.log(id);
+            if(id.includes("#")){
+                const group_id = parseInt(id.split("#")[1]) - thread;
+                // Get the sub-group
+                if(id.includes("-")){
+                    const subgroup_id = parseInt(id.split("-")[1]);
+                    const sub_group = sub_group_data.find(g => g['Group'] === group_id && g['SubGroup'] === subgroup_id);
+                    if(sub_group){
+                        const view = new KeyPhraseView(sub_group, cluster_docs);
+                    }
+                }else{
+                    const found = id.match(/#/g);
+                    if(found && found.length === 2){
+                        // This indicates the groups has only one subgroup. so we use the group data.
+                        // Get the group
+                        const group = group_data.find(g => g['Group'] === group_id);
+                        // Display the group
+                        const view = new KeyPhraseView(group, cluster_docs);
+                    }
+                }
+            }
+        });// End of chart onclick event
+
+
+
+    }
+
+    create_UI();
+}
---
+++ frontend/js/term_sunburst/key_phrase_view.js
@@ -0,0 +1,203 @@
+// Create a div to display a sub-group of key phrases
+function KeyPhraseView(sub_group, cluster_docs){
+    const sub_group_doc_ids = sub_group['DocIds'];
+    const key_phrases = sub_group['Key-phrases'];
+    const title_words = sub_group['TitleWords'];
+    console.log(key_phrases);
+    // Get sub_group docs
+    const sub_group_docs = cluster_docs.filter(d => sub_group_doc_ids.includes(d['DocId']))
+    console.log(sub_group_docs);
+    const dict = create_key_phrases_dict(title_words, key_phrases);
+
+    // Create a dict of word and key phrases
+    // Allocate the key phrases based on the words
+    function create_key_phrases_dict(title_words, key_phrases){
+        let dict = {};
+        // Initialise dict with an array
+        for(const title_word of title_words){
+            dict[title_word] = [];
+        }
+        // Add misc
+        dict['Others'] = [];
+        // Match the key phrases with title words and assign key phrase
+        let max_count = 0;
+        for(const key_phrase of key_phrases) {
+            let is_found = false;
+            for(const title_word of title_words){
+                if(key_phrase.toLowerCase().includes(title_word.toLowerCase())){
+                    dict[title_word].push(key_phrase);
+                    is_found = true;
+                }
+            }
+            // If not found, assign to 'misc'
+            if(!is_found){
+                dict['Others'].push(key_phrase);
+            }
+        }
+        return dict;
+    }
+
+    // Display the relevant papers containing key phrases
+    function display_papers_by_word(word){
+        const relevant_key_phrases = dict[word];
+        const relevant_docs = sub_group_docs.filter(d => {
+            for(const key_phrase of relevant_key_phrases){
+                const found = d['KeyPhrases'].find(kp => kp.toLowerCase() === key_phrase.toLowerCase());
+                if(found){
+                    return true;
+                }
+            }
+            return false;
+        })
+
+        // Display all the relevant papers.
+        const header_text = word;
+        // Create a list view of docs
+        const doc_list = new DocList(relevant_docs, relevant_key_phrases, header_text);
+    }
+
+    // Display the relevant papers containing key phrases
+    function display_papers_by_key_phrase(key_phrase){
+        const relevant_docs = sub_group_docs.filter(d => {
+            const found = d['KeyPhrases'].find(kp => kp.toLowerCase() === key_phrase.toLowerCase());
+            if(found){
+                return true;
+            }
+            return false;
+        })
+
+        // Display all the relevant papers.
+        const header_text = key_phrase;
+        // Create a list view of docs
+        const doc_list = new DocList(relevant_docs, [key_phrase], header_text);
+    }
+
+    // Create title word header
+    function create_header(key_phrase_div){
+        // Create a header
+        const header_div = $('<div></div>');
+        const word_list = title_words.concat(['Others']);
+        for(const word of word_list){
+            const relevant_key_phrases = dict[word];
+            const btn = $('<button type="button" class="btn btn-link">' +
+                '<span class="fw-bold text-capitalize">' + word + '</span> </button>');
+            btn.button();
+            // click event for sub_group_btn
+            btn.click(function(event){
+                // Display a list of relevant key phrases
+                key_phrase_div.empty();
+                const list_group = $('<div class="mx-auto"></div>');
+                // Add each key phrase
+                for(const key_phrase of relevant_key_phrases){
+                    const kp_btn = $('<button type="button" class="btn">' +
+                        '<span class="badge rounded-pill bg-light text-dark">' + key_phrase+ '</span></button>');
+                    // Add button
+                    kp_btn.button({
+                        classes: {
+                            "ui-button": "highlight"
+                        }
+                    });
+                    // On click event
+                    kp_btn.click(function(event){
+                        display_papers_by_key_phrase(key_phrase);
+                    });
+                    list_group.append(kp_btn);
+                }
+                key_phrase_div.append(list_group);
+                // Display the list of papers containing the key phrases
+                display_papers_by_word(word);
+
+            });
+            header_div.append(btn);
+        }
+
+        return header_div;
+    }
+
+    // Display papers for each individual key phrase
+    function display_paper_by_key_phrase(key_phrase){
+        const docs = sub_group_docs.filter(d => {
+            const found = d['KeyPhrases'].find(kp => kp.toLowerCase() === key_phrase.toLowerCase())
+            return found;
+        });
+        console.log(docs);
+        // Create a list view of docs
+        const doc_list = new DocList(docs, [key_phrase], key_phrase);
+    }
+
+    // Create an list item to display a group of key phrases
+    function createSubGroupView(){
+        // Create a container
+        const container = $('<div></div>');
+        const key_phrase_div = $('<div></div>');
+
+        // Add the header div to display the title words
+        const header_div = create_header(key_phrase_div);
+        container.append(header_div);
+        container.append(key_phrase_div);
+
+        // const header_div = create_header();
+        // container.append(header_div);
+        // // Create a table
+        // const table_div = $('<table class="table table-bordered table-sm">' +
+        //                     '<tbody></tbody></table>');
+        // const table_header_div = create_table_header();
+        // // Append div
+        // table_div.append(table_header_div);
+        // // Create table body
+        // // Get all the key-phrases
+        // const [key_phrase_dict, max_count] = create_key_phrases_dict(key_phrases, title_words);
+        // console.log(key_phrase_dict);
+        // // Add the key-phrases
+        // for(let i = 0; i < max_count; i++){
+        //     // Get the
+        //     const row = $('<tr></tr>');
+        //     for(const word of title_words){
+        //         const key_phrase = key_phrase_dict[word][i];
+        //         const col = $('<td></td>');
+        //         if(key_phrase){
+        //             const col_btn = $('<a class="ui-widget ui-corner-all">'  + key_phrase+ ' </a>');
+        //             col_btn.button();
+        //             col_btn.click(function(event){
+        //                 display_paper_by_key_phrase(key_phrase);
+        //             });
+        //             // col.text(key_phrase);
+        //             col.append(col_btn);
+        //         }
+        //         row.append(col);
+        //     }
+        //     // Add misc
+        //     const key_phrase = key_phrase_dict['misc'][i];
+        //     const col = $('<td></td>');
+        //     if(key_phrase){
+        //         const col_btn = $('<a class="ui-widget ui-corner-all">'  + key_phrase+ ' </a>');
+        //         col_btn.button();
+        //         col_btn.click(function(event){
+        //             display_paper_by_key_phrase(key_phrase);
+        //         });
+        //         col.append(col_btn);
+        //     }
+        //     row.append(col);
+        //     table_div.append(row);
+        // }
+        // container.append(table_div);
+        return container;
+    }
+
+    // Create UI
+    function _createUI(){
+        const p = $('<p></p>');
+        // A list of grouped key phrases
+        const div = createSubGroupView();
+        p.append(div);
+        $('#sub_group').empty();
+        $('#sub_group').append(p);
+        $('#doc_list').empty();
+        // Create a list of docs
+        const doc_list = new DocList(sub_group_docs, key_phrases, title_words.join(", "));
+        document.getElementById('sub_group').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    }
+
+
+    _createUI();
+}
